216e508cf7aac8a82c4e28434612d931a3fb56f8
karthi-anubavam <karthi@anubavam.com>
2013-06-24 16:38:22 +0530

    Menu Item Added

--- gen/com/example/sqlitedemo/R.java
+++ gen/com/example/sqlitedemo/R.java
@@ -21,22 +21,22 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int arrow_white=0x7f020000;
-        public static final int edit_icon=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int add_icon=0x7f020000;
+        public static final int arrow_white=0x7f020001;
+        public static final int edit_icon=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000a;
-        public static final int addBtn=0x7f080000;
-        public static final int addressText=0x7f080004;
-        public static final int editBtn=0x7f080009;
-        public static final int phoneNo=0x7f080003;
-        public static final int saveBtn=0x7f080005;
-        public static final int userAddress=0x7f080008;
-        public static final int userId=0x7f080006;
-        public static final int userName=0x7f080002;
-        public static final int userPhNo=0x7f080007;
-        public static final int user_list=0x7f080001;
+        public static final int add_button=0x7f080009;
+        public static final int addressText=0x7f080003;
+        public static final int editBtn=0x7f080008;
+        public static final int phoneNo=0x7f080002;
+        public static final int saveBtn=0x7f080004;
+        public static final int userAddress=0x7f080007;
+        public static final int userId=0x7f080005;
+        public static final int userName=0x7f080001;
+        public static final int userPhNo=0x7f080006;
+        public static final int user_list=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/sqlitedemo/MainActivity.java
+++ src/com/example/sqlitedemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -15,20 +16,20 @@ import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends Activity {
-	
+
 	LazyAdapter adapter;
 	ListView lv;
-	DatabaseHandler db; 
-	
+	DatabaseHandler db;
+
 	public MainActivity() {
 		db = new DatabaseHandler(this);
 	}
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		/*
 		Button addBtn = (Button) findViewById(R.id.addBtn);
 
 		addBtn.setOnClickListener(new View.OnClickListener() {
@@ -44,24 +45,27 @@ public class MainActivity extends Activity {
 				startActivity(i);
 			}
 		});
-
-		lv = (ListView) findViewById(R.id.user_list);
+		*/
 		
-        lv.setOnItemClickListener(new OnItemClickListener() {
+		lv = (ListView) findViewById(R.id.user_list);
+
+		lv.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
 				// TODO Auto-generated method stub
 				/*
-				String userId = ((TextView) view.findViewById(R.id.userId)).getText().toString();
-				
-				Intent i = new Intent(getApplicationContext(), AddAddress.class);
-				i.putExtra("type", "edit");
-				i.putExtra("userId", userId);
-				startActivity(i);
-				
-				Toast.makeText(getApplicationContext(), "userId = "+userId, Toast.LENGTH_SHORT).show();
-				*/
+				 * String userId = ((TextView)
+				 * view.findViewById(R.id.userId)).getText().toString();
+				 * 
+				 * Intent i = new Intent(getApplicationContext(),
+				 * AddAddress.class); i.putExtra("type", "edit");
+				 * i.putExtra("userId", userId); startActivity(i);
+				 * 
+				 * Toast.makeText(getApplicationContext(), "userId = "+userId,
+				 * Toast.LENGTH_SHORT).show();
+				 */
 			}
 		});
 	}
@@ -71,16 +75,40 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+
 	}
-	
-	public void refreshList(){
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+		case R.id.add_button:
+			// Single menu item is selected do something
+			// Ex: launching new activity/screen or show alert message
+			Toast.makeText(getApplicationContext(), "Add button clicked",
+					Toast.LENGTH_LONG).show();
+
+			Intent i = new Intent(getApplicationContext(), AddAddress.class);
+			i.putExtra("type", "new");
+			startActivity(i);
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public void refreshList() {
 		ListView lv = (ListView) findViewById(R.id.user_list);
 		/*
-		ListAdapter adapter = new SimpleAdapter(
-				this, db.getUserObjList(),
-				R.layout.user_info, new String[] { DatabaseHandler.KEY_ID, DatabaseHandler.KEY_NAME, DatabaseHandler.KEY_PH_NO, DatabaseHandler.KEY_ADDRESS }, new int[] {R.id.userId, R.id.userName, R.id.userPhNo, R.id.userAddress });
-		*/
-		LazyAdapter adapter = new LazyAdapter(getApplicationContext(), lv, MainActivity.this, db.getUserObjList());  
+		 * ListAdapter adapter = new SimpleAdapter( this, db.getUserObjList(),
+		 * R.layout.user_info, new String[] { DatabaseHandler.KEY_ID,
+		 * DatabaseHandler.KEY_NAME, DatabaseHandler.KEY_PH_NO,
+		 * DatabaseHandler.KEY_ADDRESS }, new int[] {R.id.userId, R.id.userName,
+		 * R.id.userPhNo, R.id.userAddress });
+		 */
+		LazyAdapter adapter = new LazyAdapter(getApplicationContext(), lv,
+				MainActivity.this, db.getUserObjList());
 		lv.setAdapter(adapter);
 	}
 
